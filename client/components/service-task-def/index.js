6e0738fd20a761f01741e23e6e33dbd7d6f681ad
Stephen Mathieson <me@stephenmathieson.com>
2016-05-25 18:36:29 -0700

    service: add task def info to sheet (#20)
    - misc style cleanup
    - add tabs to the sheet
    - put "events" in a tab
    - add task def stuff tab

---
+++ client/components/service-task-def/index.js
@@ -0,0 +1,47 @@
+
+import React, { Component } from 'react';
+import moment from 'moment';
+import classname from 'classname';
+import styles from './index.css';
+
+export default class ServiceTaskDef extends Component {
+  render() {
+    const { family, revision, definition } = this.props;
+    return (
+      <div className={styles.ServiceTaskDef}>
+        <table>
+          <tbody>
+            <tr>
+              <th>task def</th>
+              <td>{family}:{revision}</td>
+            </tr>
+            <tr>
+              <th>CPU</th>
+              <td>{definition.cpu}</td>
+            </tr>
+            <tr>
+              <th>memory</th>
+              <td>{definition.memory}</td>
+            </tr>
+            <tr>
+              <th>command</th>
+              <td>
+                <code>{definition.command.join(' ')}</code>
+              </td>
+            </tr>
+            <tr>
+              <th>environment</th>
+              <td>
+                <ul className={styles.ServiceTaskDefEnvVars}>
+                  {definition.environment.map(({ name, value }) =>
+                    <li key={`__${family}_${revision}_env_${name}`}><code>{name}={value}</code></li>
+                  )}
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
