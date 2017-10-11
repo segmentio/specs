dc1b231b9449d61443d10f22ff26fe25ee2d3f2c
Kevin Burke <kev@inburke.com>
2017-10-10 18:32:55 -0700

    Add link to AWS console (#40)
    * Add link to AWS console
    
    A link should make it easier to see more details about the task
    definition, and click around to related resources in the AWS console.
    
    * Use AWS_REGION environment variable
    
    * Revert port changes

--- client/components/service-task-def/index.js
+++ client/components/service-task-def/index.js
@@ -4,6 +4,8 @@ import moment from 'moment';
 import classname from 'classname';
 import styles from './index.css';
 
+const awsRegion = process.env.AWS_REGION
+
 export default class ServiceTaskDef extends Component {
   render() {
     const { family, revision, definition } = this.props;
@@ -14,7 +16,11 @@ export default class ServiceTaskDef extends Component {
           <tbody>
             <tr>
               <th>task def</th>
-              <td>{family}:{revision}</td>
+              <td>
+                <a href={`https://${awsRegion}.console.aws.amazon.com/ecs/home?region=${awsRegion}#/taskDefinitions/${family}/${revision}`}>
+                  {family}:{revision}
+                </a>
+              </td>
             </tr>
             <tr>
               <th>CPU</th>
